84976bd25f486d394d858baa19be62632d971f27
robin@noamd <robinppt@outlook.com>
2026-01-08 16:43:55 +0200

    switch to fedora

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,17 +8,67 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+#Cachy Kernels
+dnf5 -y copr enable bieszczaders/kernel-cachyos-lto
+
+# RPMfusion repos
+dnf5 -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
+
+dnf5 config-manager setopt install_weak_deps=0
+
+dnf5 remove -y --allow-erasing \
+	       --disable-plugin=protected_packages \
+	       kernel \
+	       kernel-core \
+	       kernel-modules \
+	       wpa_supplicant \
+	       sudo \
+	       pulseaudio
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+dnf5 install -y --setopt keepcache=1 \
+		kakoune \
+		fish \
+		kernel-cachyos-lto\
+		f2fs-tools\
+		composefs-rs \
+        	gdm \
+        	gnome-shell \
+        	flatpak \
+        	ananicy-cpp \
+        	mesa-dri-drivers \
+        	systemd-boot \
+        	xdg-desktop-portal-gnome \
+        	xdg-user-dirs-gtk \
+        	iwd \
+        	systemd-resolved \
+        	pipewire \
+        	wireplumber \
+        	NetworkManager-wifi \
+        	plymouth 
 
 systemctl enable podman.socket
+
+systemctl enable gdm ananicy-cpp iwd NetworkManager systemd-resolved
+
+systemctl set-default graphical.target
+
+
+
+setsebool -P domain_kernel_load_modules on
+
+ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+mkdir -p /etc/NetworkManager/conf.d /etc/kernel /etc/dracut.conf.d
+
+echo -e "[device]\nwifi.backend=iwd" > /etc/NetworkManager/conf.d/wifi_backend.conf
+
+echo 'add_dracutmodules+=" f2fs "' > /etc/dracut.conf.d/f2fs.conf
+
+echo "composefs=1 rootfstype=f2fs rw preempt=full quiet rhgb" >> /etc/kernel/cmdline
+
+kernel-install add-all
+
+
